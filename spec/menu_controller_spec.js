d35e9ba6fcf4a49ae4499432ed82053e70be2844
julesburn <cmbenson88@gmail.com>
2019-04-09 13:41:02 -0500

    Assignment 2 Complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -19,4 +19,12 @@ describe("MenuController", () => {
       expect(this.menu.getContactCount()).toBe(1)
     });
   });
+//#3
+  describe("#remindMe()", () => {
+
+    it("should returnpm  'Learning is a life-long pursuit'", () =>{
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+    });
+
+  });
 });
